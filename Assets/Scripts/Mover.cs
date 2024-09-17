eb203347d1db2617a4322f07e8386255c1493e4c
Befo <pbang2408@gmail.com>
2024-09-17 16:16:00 +0700

    Add player model and create animation blend tree, change from click to move to click or hold to move

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,15 +5,14 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButton(1))
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     void MoveToCursor()
@@ -25,5 +24,14 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        
     }
 }
