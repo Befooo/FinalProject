78f9228e515ea4eee5f13908a097b44277cdffb5
Befo <pbang2408@gmail.com>
2024-09-21 13:41:50 +0700

    Import new animations for attacking and implement attack behaviour
    Player can attack and deal damage to the target

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,18 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Core;
 using UnityEngine;
 using RPG.Movement;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
         
         private Transform target;
+        private float timeSinceLastAttack = 0;
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if (target == null) return;
             if (!GetIsInRange())
             {
@@ -20,11 +25,26 @@ namespace RPG.Combat
             }
             else
             {
-                GetComponent<Mover>().Stop();
-                target = null;
+                GetComponent<Mover>().Cancel();
+                AttackBehavior();
             }
         }
 
+        private void AttackBehavior()
+        {
+            if(timeSinceLastAttack > timeBetweenAttacks)
+            {
+                // Trigger Hit()
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+        }
+        // Animation Event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
+        }
         private bool GetIsInRange()
         {
             return Vector3.Distance(transform.position, target.position) < weaponRange;
@@ -32,11 +52,13 @@ namespace RPG.Combat
 
         public void Attack(CombatTarget combatTarget)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.transform;
         }
         public void Cancel()
         {
             target = null;
         }
+
     }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
+using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour
+    public class Mover : MonoBehaviour, IAction
     {
         [SerializeField] Transform target;
         private NavMeshAgent navMeshAgent;
@@ -22,10 +23,11 @@ namespace RPG.Movement
             UpdateAnimator();
         }
 
-        public void Stop()
+        public void Cancel()
         {
             navMeshAgent.isStopped = true;
         }
+        
         public void MoveTo(Vector3 destination)
         {
             navMeshAgent.destination = destination;
@@ -41,7 +43,7 @@ namespace RPG.Movement
         }
         public void StartMoveAction(Vector3 destination)
         {
-            GetComponent<Fighter>().Cancel();
+            GetComponent<ActionScheduler>().StartAction(this);
             MoveTo(destination);
         }
     }
