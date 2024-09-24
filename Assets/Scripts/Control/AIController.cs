bdfd6c70f15a461b0bf0e4bbb5eeda8bc4326e92
Befo <pbang2408@gmail.com>
2024-09-24 18:51:06 +0700

    Implement guarding and suspicion behaviour for enemy AI
    When player not in chase range enemy will enter suspicion behaviour for few seconds and then turn back to guarding behaviour (the last position before chasing)

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -4,20 +4,31 @@ using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
 using UnityEngine;
+using RPG.Movement;
 
 namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 3f;
+        
         private Fighter fighter;
         private Health health;
+        private Mover mover;
         GameObject player;
+        
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
         private void Start()
         {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
             player = GameObject.FindWithTag("Player");
+            mover = GetComponent<Mover>();
+            
+            guardPosition = transform.position;
+            
         }
 
         private void Update()
@@ -25,18 +36,45 @@ namespace RPG.Control
             if (health.IsDead) return;
             if (InAttackRange() && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
             }
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         private bool InAttackRange()
         {
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
     }
 }
