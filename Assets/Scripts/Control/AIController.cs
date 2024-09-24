98482438d7372bed7391d984f19280ca55633b9b
Befo <pbang2408@gmail.com>
2024-09-24 18:10:08 +0700

    Implement enemy AI and bug fixing
    Enemy will start chasing if the player is in chase range and start attacking if player is in attack range, fix bug that player can still move around after dead and enemy can still chasing the player after dead

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Combat;
+using RPG.Core;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+        private Fighter fighter;
+        private Health health;
+        GameObject player;
+        private void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            health = GetComponent<Health>();
+            player = GameObject.FindWithTag("Player");
+        }
+
+        private void Update()
+        {
+            if (health.IsDead) return;
+            if (InAttackRange() && fighter.CanAttack(player))
+            {
+                fighter.Attack(player);
+            }
+            else
+            {
+                fighter.Cancel();
+            }
+        }
+
+        private bool InAttackRange()
+        {
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            return distanceToPlayer < chaseDistance;
+        }
+    }
+}
