4dd7bdd30c7f474925736a7265d3ac81d2b0915e
nobi-onway <doangiabao.dev@gmail.com>
2024-11-08 01:21:13 +0700

    add custom cursor

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 using RPG.Movement;
 using RPG.Combat;
 using RPG.Core;
+using System.Security.Cryptography;
 
 namespace RPG.Control
 {
+    public enum ECursorType { NONE, MOVEMENT, COMBAT }
+
+    [Serializable]
+    struct CursorMapping
+    {
+        public ECursorType cursorType;
+        public Texture2D texture;
+        public Vector2 hotspot;
+    }
     public class PlayerController : MonoBehaviour
     {
         Health health;
+        [SerializeField] private CursorMapping[] _cursorMappings = null;
         private void Awake()
         {
             health = GetComponent<Health>();
@@ -21,6 +32,8 @@ namespace RPG.Control
             if (health.IsDead) return;
             if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
+
+            SetCursor(ECursorType.NONE);
         }
 
         bool InteractWithCombat()
@@ -35,10 +48,28 @@ namespace RPG.Control
                 {
                     GetComponent<Fighter>().Attack(target.gameObject);
                 }
+                SetCursor(ECursorType.COMBAT);
                 return true;
             }
             return false;
         }
+
+        private void SetCursor(ECursorType cursorType)
+        {
+            CursorMapping mapping = GetCursorMapping(cursorType);
+            Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping(ECursorType cursorType)
+        {
+            foreach (CursorMapping mapping in _cursorMappings)
+            {
+                if (mapping.cursorType == cursorType) return mapping;
+            }
+
+            return _cursorMappings[0];
+        }
+
         private bool InteractWithMovement()
         {
             RaycastHit hit;
@@ -49,6 +80,7 @@ namespace RPG.Control
                 {
                     GetComponent<Mover>().StartMoveAction(hit.point, 1f);
                 }
+                SetCursor(ECursorType.MOVEMENT);
                 return true;
             }
             return false;
