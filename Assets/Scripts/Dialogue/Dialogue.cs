7edb9d1898eca7a2e004ce324d69eb118b6e0f36
nobi-onway <doangiabao.dev@gmail.com>
2024-11-30 11:42:47 +0700

    craete dragging node feature Dialogue Editor

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -6,7 +6,27 @@ namespace RPG.Dialogue
     [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
     public class Dialogue : ScriptableObject
     {
-        [SerializeField] private DialogueNode[] nodes;
+        [SerializeField] private List<DialogueNode> nodes = new List<DialogueNode>();
+
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            if (nodes.Count == 0)
+            {
+                nodes.Add(new DialogueNode());
+            }
+        }
+#endif
+
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
+
+        public DialogueNode GetRootNode()
+        {
+            return nodes[0];
+        }
     }
 }
 
--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,5 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Codice.Client.Commands;
+using Codice.Client.Common.TreeGrouper;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
@@ -8,6 +9,10 @@ namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selectedDialogue = null;
+        GUIStyle nodeStyle;
+        DialogueNode draggingNode;
+
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
         {
@@ -19,17 +24,105 @@ namespace RPG.Dialogue.Editor
             Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
             if (dialogue != null)
             {
-                ShowEditorWindow(); 
+                ShowEditorWindow();
                 return true;
             }
-            
+
             return false;
         }
+
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectedChange;
+
+            nodeStyle = new GUIStyle();
+            nodeStyle.normal.background = EditorGUIUtility.Load("node0") as Texture2D;
+            nodeStyle.padding = new RectOffset(20, 20, 20, 20);
+            nodeStyle.border = new RectOffset(12, 12, 12, 12);
+        }
+
+        private void OnSelectedChange()
+        {
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+
+            if (newDialogue != null)
+            {
+                selectedDialogue = newDialogue;
+                Repaint();
+            }
+        }
+
         private void OnGUI()
         {
-            EditorGUILayout.LabelField("Hello");
-            EditorGUILayout.LabelField("Hello 2");
-            EditorGUILayout.LabelField("Hello 3");
+            if (selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected");
+            }
+            else
+            {
+                ProcessEvents();
+                foreach (DialogueNode dialogue in selectedDialogue.GetAllNodes())
+                {
+                    OnGUINode(dialogue);
+                }
+            }
+        }
+
+        private void ProcessEvents()
+        {
+            Vector2 draggingOffset = Vector2.zero;
+
+            if (Event.current.type == EventType.MouseDown && draggingNode == null)
+            {
+                draggingNode = GetNodeAtPoint(Event.current.mousePosition);
+
+                if (draggingNode != null)
+                {
+                    draggingOffset = draggingNode.rect.position - Event.current.mousePosition;
+                } 
+            }
+            else if (Event.current.type == EventType.MouseDrag && draggingNode != null)
+            {
+                Undo.RecordObject(selectedDialogue, "Move dialogue node");
+                draggingNode.rect.position = Event.current.mousePosition + draggingOffset;
+                GUI.changed = true;
+            }
+            else if (Event.current.type == EventType.MouseUp && draggingNode != null)
+            {
+                draggingNode = null;
+            }
+        }
+
+        private DialogueNode GetNodeAtPoint(Vector2 point)
+        {
+            DialogueNode foundNode = null;
+
+            foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+            {
+                if (node.rect.Contains(point)) foundNode = node;
+            }
+
+            return foundNode;
+        }
+
+        private void OnGUINode(DialogueNode dialogue)
+        {
+            GUILayout.BeginArea(dialogue.rect, nodeStyle);
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.LabelField("Node: ", EditorStyles.boldLabel);
+            string newText = EditorGUILayout.TextField(dialogue.text);
+            string newUniqueID = EditorGUILayout.TextField(dialogue.uniqueID);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(selectedDialogue, "Update Dialogue Text");
+
+                dialogue.text = newText;
+                dialogue.uniqueID = newUniqueID;
+            }
+
+            GUILayout.EndArea();
         }
     }
 }
